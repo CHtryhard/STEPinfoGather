6d1fe7119c60865c031c42c2dbd8624b32441d5b
CHtryhard <38825242+CHtryhard@users.noreply.github.com>
2024-07-21 22:50:03 -0500

    need to finalize the function

--- STEPinforExtrator/STEPinforExtrator/MainFrame.cpp
+++ STEPinforExtrator/STEPinforExtrator/MainFrame.cpp
@@ -2,8 +2,14 @@
 #include <wx/dirctrl.h>
 #include <wx/treectrl.h>
 #include <wx/dir.h>
-#include <fstream>
 #include "STEP__Converter.h"
+
+#include <iostream>
+#include <string>
+#include <filesystem> // C++17 and later
+#include <algorithm>
+
+namespace fs = std::filesystem;
 
 MyFrame::MyFrame(const wxString& title)
     : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(600, 400))
@@ -31,6 +37,7 @@ MyFrame::MyFrame(const wxString& title)
 
 void MyFrame::OnOpenFolder(wxCommandEvent& event)
 {
+    fileListTextCtrl->Clear();
     wxDirDialog openDirDialog(this, "Choose a directory", "", wxDD_DEFAULT_STYLE | wxDD_DIR_MUST_EXIST);
     if (openDirDialog.ShowModal() == wxID_CANCEL)
         return;
@@ -66,11 +73,40 @@ void MyFrame::OnConfirmButton(wxCommandEvent& event)
 {
     std::string path;
     path = selectedPath.ToStdString();
-    
-    if (processStepFile(path) == -1) {
-        wxLogMessage("Error reading STEP file");
-    }
-    else {
-        wxLogMessage("Success");
-    }
+
+    //wxDir dir(selectedPath);
+
+    //wxString filename;
+
+    //bool cont = dir.GetFirst(&filename, wxEmptyString, wxDIR_FILES);
+    //while (cont)
+    //{
+    //    cont = dir.GetNext(&filename);
+    //}
+
+    //if (processStepFile(path) == -1) {
+    //    wxLogMessage("Error reading STEP file");
+    //}
+    //else {
+    //    wxLogMessage("Success");
+    //}
+ 
+    //initialize file to write
+    std::ofstream outputfile;
+    std::time_t currentTime = std::time(nullptr);
+    // Initialize a tm structure
+    std::tm localTime;
+    // Convert to local time using localtime_s
+    localtime_s(&localTime, &currentTime);
+    // Extract the year, month, day, hour, minute
+    int year = localTime.tm_year + 1900; // tm_year is years since 1900
+    int month = localTime.tm_mon + 1;    // tm_mon is 0-based, so add 1
+    int day = localTime.tm_mday;
+    int hour = localTime.tm_hour;
+    int minute = localTime.tm_min;
+    std::string outputpath = path +"//" + std::to_string(year) + "-" + std::to_string(month) + "-" + std::to_string(day) + "__" + std::to_string(hour) + std::to_string(minute) + ".csv";
+    outputfile.open(outputpath, std::ios::app);
+    //write header
+    outputfile << "File name,X,Y,Z,BV/5000,SV,Density,Weight,QTY" << std::endl;
+    outputfile.close();
 }
--- STEPinforExtrator/STEPinforExtrator/STEP__Converter.h
+++ STEPinforExtrator/STEPinforExtrator/STEP__Converter.h
@@ -1,33 +1,12 @@
 #include <STEPControl_Reader.hxx>
-#include <TopExp_Explorer.hxx>
-#include <BRep_Tool.hxx>
 #include <BRepGProp.hxx>
 #include <GProp_GProps.hxx>
 #include <Bnd_Box.hxx>
 #include <BRepBndLib.hxx>
 #include <Interface_Static.hxx>
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <fstream>
 
 
-int processStepFile(const std::string& filename) {
-    //initialize file to write
-    std::ofstream myfile;
-    std::time_t currentTime = std::time(nullptr);
-    // Initialize a tm structure
-    std::tm localTime;
-    // Convert to local time using localtime_s
-    localtime_s(&localTime, &currentTime);
-    // Extract the year, month, day, hour, minute
-    int year = localTime.tm_year + 1900; // tm_year is years since 1900
-    int month = localTime.tm_mon + 1;    // tm_mon is 0-based, so add 1
-    int day = localTime.tm_mday;
-    int hour = localTime.tm_hour;
-    int minute = localTime.tm_min;
-    std::string outputname = std::to_string(year) + "-" + std::to_string(month) + "-" + std::to_string(day) + "__" + std::to_string(hour) + std::to_string(minute) + ".csv";
-    myfile.open(outputname);
+int processStepFile(const std::string& filename, std::ofstream& myfile) {
 
     // Load STEP file
     STEPControl_Reader reader;
@@ -52,21 +31,18 @@ int processStepFile(const std::string& filename) {
     // Get the bounding box dimensions
     Standard_Real xMin, yMin, zMin, xMax, yMax, zMax;
     boundingBox.Get(xMin, yMin, zMin, xMax, yMax, zMax);
-    Standard_Real BVolume = (xMax - xMin) * (yMax - yMin) * (zMax - zMin);
+    Standard_Real BVolume = (xMax - xMin) * (yMax - yMin) * (zMax - zMin) / 1000;
 
     // Calculate the volume
     GProp_GProps properties;
     BRepGProp::VolumeProperties(shape, properties);
-    double volume = properties.Mass();
+    double volume = properties.Mass() / 1000;
     //write file
-    myfile << "X,Y,Z,BV/5000,SV,Density,Weight,QTY\n";
-    myfile << xMax - xMin << ",";
-    myfile << yMax - yMin << ",";
-    myfile << zMax - zMin << ",";
-    myfile << BVolume/5000 << ",";
-    myfile << volume << ","<<"," << "," << std::endl;
-    myfile.close();
+    myfile << filename << ",";
+    myfile << (xMax - xMin) / 10 << ",";
+    myfile << (yMax - yMin) / 10 << ",";
+    myfile << (zMax - zMin) / 10 << ",";
+    myfile << BVolume / 5000 << ",";
+    myfile << volume << "," << "," << "," << std::endl;
     return 0;
 }
-
-
